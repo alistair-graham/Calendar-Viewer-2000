ef564420b345e5edec1eb28e20d44b58e413d0ec
Annette <annette.graham@sky.uk>
2023-02-10 03:53:06 +0000

    [Alistair] Shoe events in corresponding table cell

--- src/components/Calendar/HourRow/HourRow.tsx
+++ src/components/Calendar/HourRow/HourRow.tsx
@@ -4,6 +4,40 @@ import { makeSelectEventsByHour } from "../../../features/calendar/calendarSlice
 import { Day } from "../../../app/types";
 
 const HourRow = (props: { hour: number }) => {
+  const eventsByHour = useAppSelector((state) =>
+    makeSelectEventsByHour()(state, props.hour)
+  );
+
+  const createRow = () => {
+    const cells: React.ReactNode[] = [];
+
+    Object.values(Day).forEach((day) => {
+      cells.push(
+        <td className="cell hour-cell" key={day + props.hour}>
+          {createEventsForDay(day)}
+        </td>
+      );
+    });
+    return cells;
+  };
+
+  const createEventsForDay = (day: Day) => {
+    const eventsForDay: React.ReactNode[] = [];
+
+    const eventsForCell = eventsByHour?.filter((event) => event.day === day);
+    const keyNameBase = day + props.hour + "-";
+
+    eventsForCell?.forEach((event, i) => {
+      eventsForDay.push(
+        <p className="event" key={keyNameBase + i}>
+          {event.name}
+        </p>
+      );
+    });
+
+    return eventsForDay;
+  };
+
   return (
     <tr className="row hour-row">
       <th className="cell first-column" scope="row">
